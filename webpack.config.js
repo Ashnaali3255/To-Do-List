d5d1e675a5dabe37050d4644b9d9a4683c3f725b
Ashnaali3255 <ashnaalipk@gmail.com>
2023-02-23 14:57:53 +0500

    Update src folder and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
@@ -10,5 +15,16 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  // optimization: {
+  // runtimeChunk: 'single',
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
